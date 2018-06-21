94a6209bdc4a8fa65e3d9a405e3a49d11f6120a2
Yuming He <yuming.p.he@gmail.com>
2018-06-20 21:45:05 -0400

    Remove unused migration scripts

--- migrations/2_home_bridge.js
+++
@@ -1,5 +0,0 @@
-var HomeBridge = artifacts.require('HomeBridge.sol');
-
-module.exports = function (deployer) {
-  deployer.deploy(HomeBridge)
-}
--- migrations/3_foreign_bridge.js
+++
@@ -1,5 +0,0 @@
-var ForeignBridge = artifacts.require('ForeignBridge.sol');
-
-module.exports = function (deployer) {
-  deployer.deploy(ForeignBridge)
-}
